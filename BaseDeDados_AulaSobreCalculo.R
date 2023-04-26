e0670c1ae62123955828afdcdcd39d7959ce91ee
Eveline Cleide <eveline.cleide@ufpe.br>
2023-04-26 16:02:46 -0300

    AulaSobreCalculo

---
+++ BaseDeDados_AulaSobreCalculo.R
@@ -0,0 +1,11 @@
+Seatbelts
+?Seatbelts
+
+poissonSeatbealts<- rpois (200, 4)
+hist(poissonSeatbealts)
+
+mean(poissonSeatbealts)
+
+poissonSeatbealtsCentral <- poissonSeatbealts - mean(poissonSeatbealts)
+
+hist(poissonSeatbealtsCentral)
