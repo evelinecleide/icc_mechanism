c0694a4f3f9d7dbf9ac4aa4dff63f12fa576731c
Eveline Cleide <eveline.cleide@ufpe.br>
2023-04-29 12:14:26 -0300

    Criando_objetos_e_funções

--- Criando_Objetos.R
+++ Criando_Objetos.R
@@ -2,7 +2,7 @@ vector1 <- c(1, 3, 5, 7, 9) #criação de objeto simples: números ímpares entr
 
 str(vector1) #mostrando a complexidade do objeto simples
 
-regCars <- lm(cyl ~ ., mtcars) #criação de objeto complexo via função (regressão)
+regCars <- lm(cyl ~ ., mtcars) #criação de objeto complexo via função de regressão
 
 str(regCars) #mostrando a complexidade do objeto complexo
 
