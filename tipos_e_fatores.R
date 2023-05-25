558c38cfe0b39d68c092ad4326a6717424371183
Eveline Cleide <eveline.cleide@ufpe.br>
2023-05-25 16:49:04 -0300

    fatores

--- tipos_e_fatores.R
+++ tipos_e_fatores.R
@@ -11,7 +11,7 @@ corBlusas <- c(1, 0, 2, 2, 0)
 recode <- c(preta = 0, cinza = 1, branca = 2)
 (corBlusas <- factor(corBlusas, levels = recode, labels = names(recode)))
 
-#Ordenando as blusas pelas médias delas. Vai da que tem menor média até a maior
+#Ordenando as blusas pelas médias dos valores delas. Vai da que tem menor média até a maior
 valor <- c(25, 50, 30, 75, 90)
 (corBlusas <- reorder(corBlusas, valor))
 
