d8eac44e6b30ee5cb46a3e2756e743aa9a4c7701
Eveline Cleide <eveline.cleide@ufpe.br>
2023-06-09 17:21:24 -0300

    testes_de_hipotese

--- teste_de_hipoteses.R
+++ teste_de_hipoteses.R
@@ -7,7 +7,6 @@ View(Facebook)
 
 ##teste de correlação entre as variáveis Paid (se o post foi pago ou não), e a variável Like (quantidade de likes)
 cor.test(Facebook$Paid, Facebook$like)
-
 #H0:variáveis não têm associação, ou seja, são independentes. p-valor (p-value) > 0.05
 #H1:variáveis têm associação, ou seja, são dependentes. p-valor (p-value) <= 0.05
 #com isso, temos que a hipótese nula foi rejeitada, visto que o p-valor for abaixo de 0,05: p-value = 0.01345
